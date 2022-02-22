c4fc8936820200929106bf196bc61357abd18524
sungchul09 <sungchul09@naver.com>
2022-02-22 18:03:26 +0900

    fix: Fix coordinate 코드 성능 개선

--- coordinate/main.js
+++ coordinate/main.js
@@ -2,6 +2,9 @@ const horizontal = document.querySelector('.horizontal');
 const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
 const container = document.querySelector('.container');
 let cnt = 0;
 
@@ -11,12 +14,18 @@ window.addEventListener('mousemove', (e) => {
     let y = e.clientY;
     tag.innerHTML = `${x}, ${y}`;
 
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
+    // vertical.style.left = `${x}px`;
+    // horizontal.style.top = `${y}px`;
+    // target.style.left = `${x}px`;
+    // target.style.top = `${y}px`;
+    // tag.style.left = `${x}px`;
+    // tag.style.top = `${y}px`;
+
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
 })
 
 window.addEventListener('click', (e) => {
