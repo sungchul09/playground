88420478400fe0a01e15bb3414cc9cf037161634
yun.seongcheol <sungchul09@naver.com>
2022-05-06 10:51:34 +0900

    feat: Add Toolbar(router-link) 스타일링

--- vue/vue-news/src/components/ToolBar.vue
+++ vue/vue-news/src/components/ToolBar.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <router-link to="/news">news</router-link>
-    <router-link to="/jobs">jobs</router-link>
-    <router-link to="/ask">ask</router-link>
+  <div class="header">
+    <router-link class="href" to="/news">news</router-link>
+    <router-link class="href" to="/jobs">jobs</router-link>
+    <router-link class="href" to="/ask">ask</router-link>
   </div>
 </template>
 
@@ -10,4 +10,33 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.header {
+  font-size: 50px;
+  font-weight: bold;
+  color: rgb(220, 20, 20);
+  background-color: rgb(24, 61, 124);
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.header .router-link-exact-active {
+  background-color: rgb(255, 247, 0);
+  color: black;
+}
+
+.header a {
+  text-decoration: none;
+  color: white;
+}
+
+.header a:hover {
+  color: black;
+}
+
+a {
+  padding: 10px;
+}
+</style>
