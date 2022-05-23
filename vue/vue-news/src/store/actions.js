1e71268095d39045d4d75f37cfe0705e0be17524
sungchul <sungchul09@naver.com>
2022-05-24 01:44:17 +0900

    feat: Add vue await

--- vue/vue-news/src/store/actions.js
+++ vue/vue-news/src/store/actions.js
@@ -1,35 +1,48 @@
 import {
-  fetchList,
-  fetchUserInfo,
-  fetchCommentItem,
+    fetchList,
+    fetchUserInfo,
+    fetchCommentItem,
+    fetchJobsList,
+    fetchAskList,
 } from '../api/index.js';
 
 export default {
-  FETCH_USER({ commit }, name) {
-    return fetchUserInfo(name)
-      .then(({ data }) => {
-        commit('SET_USER', data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-  FETCH_ITEM({ commit }, id) {
-    return fetchCommentItem(id)
-      .then(({ data }) => {
-        commit('SET_ITEM', data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-  FETCH_LIST({ commit }, pageName) {
-    return fetchList(pageName)
-      .then((response) => {
-        console.log(4)
-        commit('SET_LIST', response.data)
-        return response
-      })
-      .catch((error) => console.log(error));
-  },
+    FETCH_USER({ commit }, name) {
+        return fetchUserInfo(name)
+            .then(({ data }) => {
+                commit('SET_USER', data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+    FETCH_ITEM({ commit }, id) {
+        return fetchCommentItem(id)
+            .then(({ data }) => {
+                commit('SET_ITEM', data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+    async FETCH_LIST(context, pageName) {
+        const response = fetchList(pageName);
+        context.commit('SET_LIST', response.data);
+        return response;
+    },
+    async FETCH_NEWS(context) {
+        const response = await fetchNewsList();
+        context.commit('SET_NEWS', response.data);
+        return response;
+    },
+async FETCH_JOBS(context) {
+        const response = await fetchJobsList();
+        context.commit('SET_JOBS', response.data);
+        return response;
+    },
+    async FETCH_ASK(context) {
+        const response = await fetchAskList();
+        context.commit('SET_ASK', response.data);
+        return response;
+    },
 };
