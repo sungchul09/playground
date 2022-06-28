eb746a148c93415220fbe2116acbb63db80f5423
sungchul09 <sungchul09@naver.com>
2022-06-28 15:50:35 +0900

    폴더재구성

---
+++ dream_coding/browser101/carrot/main.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('.btn');
+const play = document.querySelector('#play');
+const stop = document.querySelector('#stop');
+const timer = document.querySelector('.timer');
+const count = document.querySelector('.count');
+
+
+btn.addEventListener('click', ()=>{
+  
+})
--- prog/location.js
+++
@@ -1,3 +0,0 @@
-const grid = document.querySelector('.grid');
-
-grid.addEventListener('click', () => {});
